08b019bea757984607776546ab7df4cfa99f829e
khaldi505 <habetlbrika69@gmail.com>
2021-07-19 15:41:31 +0100

    1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+"""
+concat: will concat two strings
+"""
+
 
 def concat(str1:str, str2:str) -> str:
+    """
+    returns str1 + str2 strings
+    """
     return "{}{}".format(str1, str2)
