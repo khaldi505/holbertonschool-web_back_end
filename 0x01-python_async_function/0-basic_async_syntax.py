ee7eef1ad32f970eae8db8a198569ed5f11ff3d9
khaldi505 <habetlbrika69@gmail.com>
2021-07-20 22:59:37 +0100

    confused monkey screaming

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-import random
-import asyncio
 """
 wait_random
 """
+import random
+import asyncio
 
 
-async def wait_random(max_delay: int=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
     generate a random float, sleep then return the random float
     """
-    rand = random.uniform(0, max_delay)
+    rand = random.uniform(0.0, max_delay)
     await asyncio.sleep(rand)
     return rand
