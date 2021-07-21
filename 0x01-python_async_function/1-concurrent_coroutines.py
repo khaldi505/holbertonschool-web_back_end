010b4552233d29badbac8df931475d25d6f635d6
khaldi505 <habetlbrika69@gmail.com>
2021-07-21 02:32:39 +0100

    another confused monkey because of the code style requirements

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,8 +10,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> typing.List[float]:
-    x = await asyncio.gather(*[wait_random(max_delay) for y in range(n)])
     """
     a7dohom lam yanem, ala khater realpython documention
     """
+    x = await asyncio.gather(*[wait_random(max_delay) for y in range(n)])
     return sorted(x)
