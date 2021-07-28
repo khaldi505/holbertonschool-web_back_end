b1a794493e0e55e462d4a3483303f20695ecb97a
khaldi505 <habetlbrika69@gmail.com>
2021-07-28 14:08:07 +0100

    2-lifo_cache.py

---
+++ 0x03-caching/2-lifo_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+placeholder
+"""
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+
+    LAST_PUT = ""
+    """
+        placeholder
+    """
+
+    # def __init__(self):
+    #     super().__init__()
+
+    def put(self, key, item):
+        """
+        placeholder
+        """
+        if key is None or item is None:
+            return
+        if (len(self.cache_data.items()) == BaseCaching.MAX_ITEMS):
+            if (key not in self.cache_data.keys()):
+                lastItem = self.LAST_PUT
+                print("DISCARD:", lastItem)
+                self.cache_data.pop(lastItem)
+
+        self.cache_data[key] = item
+        self.LAST_PUT = key
+
+    def get(self, key):
+        """gets the required element by key"""
+        if key not in self.cache_data.keys():
+            return None
+        else:
+            return self.cache_data[key]
