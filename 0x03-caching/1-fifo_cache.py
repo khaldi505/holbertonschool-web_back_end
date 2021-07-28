92824b276e9c2c1973c512fc85d36504a4f3b74e
khaldi505 <habetlbrika69@gmail.com>
2021-07-28 03:25:46 +0100

    1-fifo_cache.py

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -18,15 +18,16 @@ class FIFOCache(BaseCaching):
         """
         placeholder
         """
-        if not (key is None or item is None):
-            if (
-                len(self.cache_data.items()) == BaseCaching.MAX_ITEMS
-                and (key not in self.cache_data.keys())
-            ):
-                firstItem = list(self.cache_data)[0]
-                print("DISCARD:", firstItem)
-                self.cache_data.pop(firstItem)
-            self.cache_data[key] = item
+        if key is None or item is None:
+            return
+        if (
+            len(self.cache_data.items()) == BaseCaching.MAX_ITEMS
+            and (key not in self.cache_data.keys())
+        ):
+            firstItem = list(self.cache_data)[0]
+            print("DISCARD:", firstItem)
+            self.cache_data.pop(firstItem)
+        self.cache_data[key] = item
 
     def get(self, key):
         """gets the required element by key"""
