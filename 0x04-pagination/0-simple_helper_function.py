bcf61cd9b619a0c248a1ec945fbc5ef6e37e4b1e
khaldi505 <habetlbrika69@gmail.com>
2021-07-30 12:25:10 +0100

    pycode style fixed

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -7,5 +7,5 @@
 def index_range(page: int, page_size: int) -> tuple:
     """
     a function that
-    """    
-    return ((page * page_size - page_size), (page *page_size))
+    """
+    return ((page * page_size - page_size), (page * page_size))
