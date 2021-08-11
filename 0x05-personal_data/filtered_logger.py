ca0938e9f5f27b03fabe86d14ee6e5aebf85909f
khaldi505 <habetlbrika69@gmail.com>
2021-08-11 11:29:12 +0100

    filtered_logger.py

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+filter_datum returns the log message:
+"""
+
+
+from typing import List
+import logging
+import re
+
+
+def filter_datum(
+            fields: List,
+            redaction: str,
+            message: str,
+            separator: str
+            ) -> str:
+    """
+    a function that hides all the personal,
+    sensitve informations.
+    """
+    for field in fields:
+        message = re.sub("{}=([a-zA-Z0-9\\/]+){}".format(field, separator),
+                         field + "=" + redaction + separator,  message)
+    return message
