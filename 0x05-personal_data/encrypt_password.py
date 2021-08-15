3d01cb336961c4f554492da1f805b3d4c4755fad
khaldi505 <habetlbrika69@gmail.com>
2021-08-15 13:15:49 +0100

    done

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -12,3 +12,13 @@ def hash_password(password: str) -> bytes:
     """
     password = bytes(password, 'utf-8')
     return bcrypt.hashpw(password, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+        let's check if the password hash matches
+    """
+    if bcrypt.checkpw(bytes(password, 'utf-8'), hashed_password):
+        return True
+    else:
+        return False
