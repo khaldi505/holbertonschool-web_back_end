5fd2193b99a0ed510fac27f03b3af22b9617898d
khaldi505 <habetlbrika69@gmail.com>
2021-08-16 20:35:36 +0100

    ain't gonna lie, ive no idea what was that

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+auth
+"""
+from flask import request
+
+
+class Auth:
+    """
+    class docc
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''
+            salut lbnet
+        '''
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        alaa khir
+        """
+        return None
