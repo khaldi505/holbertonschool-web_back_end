4672b39fc93644a6797c2246e886769da95224e5
khaldi505 <habetlbrika69@gmail.com>
2021-08-16 11:16:29 +0100

    unauthorized update

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> str:
+  """
+  unauthorized
+  """
+  return abort(401)
