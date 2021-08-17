0533633a9b3114a07c383bdfb041278abd314b32
khaldi505 <habetlbrika69@gmail.com>
2021-08-17 16:55:43 +0100

    Request validation, task 5

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -7,11 +7,34 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-
+import sys
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth = os.getenv("AUTH_TYPE")
+if auth:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def before_request():
+    unauthorized = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+        ]
+    if not auth:
+        return
+    if not auth.require_auth(request.path, unauthorized):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    elif auth.current_user(request) is None:
+        abort(403)
 
 
 @app.errorhandler(404)
