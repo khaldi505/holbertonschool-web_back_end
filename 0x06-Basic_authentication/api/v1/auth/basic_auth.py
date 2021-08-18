bc6a623819760564efd33a10129ffa91b19237c6
Iheb khaldi <53900877+khaldi505@users.noreply.github.com>
2021-08-18 15:53:44 +0100

    Update basic_auth.py

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,7 +8,23 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """
-        for now this class litearlly does nothing
+        for now this class
+        litearlly does nothing
         except inherits from auth
     """
-    pass
+    def extract_base64_authorization_header(
+        self,
+        authorization_header: str
+            ) -> str:
+        """
+        Basic - Base64 part
+
+        """
+        if (
+            type(authorization_header) != str or
+            authorization_header is None or
+            authorization_header[0:6] != "Basic "
+        ):
+            return None
+        else:
+            return authorization_header[6:len(authorization_header)]
