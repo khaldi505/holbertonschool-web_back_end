23ec8325d300958327398b0abb9850b142bc5632
khaldi505 <habetlbrika69@gmail.com>
2021-08-24 13:38:00 +0100

    user id for session id

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -10,6 +10,7 @@ class SessionAuth(Auth):
     """
     just inherits from auth
     """
+
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
@@ -21,15 +22,15 @@ class SessionAuth(Auth):
         if type(user_id) is not str:
             return None
         session_id = str(uuid.uuid4())
-        self.user_id_by_session_id = {session_id: user_id}
+        self.user_id_by_session_id[session_id] = user_id
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """
-            yes documention
+        Returns a User ID based on a Session ID.
         """
-        if type(session_id) is not str:
-            return None
         if session_id is None:
+            return None
+        if type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
