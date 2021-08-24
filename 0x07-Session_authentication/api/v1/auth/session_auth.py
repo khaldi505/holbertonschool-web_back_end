2fac0ba8dc5775c36386d14ea71f0ee6e0caa9d7
khaldi505 <habetlbrika69@gmail.com>
2021-08-24 12:50:11 +0100

    session auth

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,23 @@
 for now this module do nothing
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     just inherits from auth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+            session auth
+        """
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id = {session_id: user_id}
+        return session_id
