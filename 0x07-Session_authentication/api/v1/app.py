20355550522f88283a7048c253aacfb2085c9875
khaldi505 <habetlbrika69@gmail.com>
2021-08-25 13:23:41 +0100

    Before request$

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -49,8 +49,6 @@ def before_request():
         return
     if not auth.require_auth(request.path, unauthorized):
         return
-    if auth.authorization_header(request) is None:
-        abort(401)
 
     elif auth.current_user(request) is None:
         abort(403)
