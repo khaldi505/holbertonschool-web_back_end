05aa44a5f9fe5a4b1cc1fbe3810ab0ac4f3ba5be
khaldi505 <habetlbrika69@gmail.com>
2021-08-27 00:53:25 +0100

    session auth updates task 7

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -2,11 +2,17 @@
 """
 for now this module do nothing
 """
+from flask.globals import current_app
+from api.v1.views.users import create_user
 from sys import stderr
+
+from flask.wrappers import Response
 from api.v1.auth.auth import Auth
 import uuid
 from models.user import User
 from os import getenv
+from flask import jsonify, abort, request
+from api.v1.views import app_views
 
 
 class SessionAuth(Auth):
@@ -46,3 +52,38 @@ class SessionAuth(Auth):
         user_id = self.user_id_for_session_id(cookie_value)
         user = User.get(user_id)
         return user
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def auth_session() -> str:
+    """
+        create an auth session
+    """
+    email = request.form.get("email", None)
+    password = request.form.get("password", None)
+
+    if (email is None or email == ""):
+        response = jsonify({"error": "email missing"})
+        response.status_code = 400
+        return response
+    if (password is None or password == ""):
+        response = jsonify({"error": "password missing"})
+        response.status_code = 400
+        return response
+    current_user = User.search({"email": email})
+    if (len(current_user) == 0 or current_user is None):
+        response = jsonify({"error": "no user found for this email"})
+        response.status_code = 404
+        return response
+
+    if not current_user[0].is_valid_password(password):
+        response = jsonify({"error": "wrong password"})
+        response.status_code = 401
+        return response
+    else:
+        from api.v1.app import auth
+        session_id = auth.create_session(current_user[0].id)
+        cookie_name = getenv('SESSION_NAME')
+        out = jsonify(current_user[0].to_json())
+        out.set_cookie(cookie_name, session_id)
+        return out
