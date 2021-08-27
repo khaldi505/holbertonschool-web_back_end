2a01e1d08fdc13a11e850582f6e576e4d117e720
khaldi505 <habetlbrika69@gmail.com>
2021-08-28 00:02:53 +0100

    user model

---
+++ 0x08-user_authentication_service/user.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+a User model for the database users.
+"""
+from flask import Flask
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.expression import true
+from sqlalchemy.sql.sqltypes import NullType
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    id, the integer primary key
+    email, a non-nullable string
+    hashed_password, a non-nullable string
+    session_id, a nullable string
+    reset_token, a nullable string
+    """
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
