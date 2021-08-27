ca54f924cb0c7c49e0cef6e61d391f18ced46574
khaldi505 <habetlbrika69@gmail.com>
2021-08-27 01:18:53 +0100

    session auth

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -2,11 +2,12 @@
 """
 for now this module do nothing
 """
-from api.v1.views import app_views
 from models.user import User
 from os import getenv
 from flask import jsonify, request
 
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def auth_session() -> str:
