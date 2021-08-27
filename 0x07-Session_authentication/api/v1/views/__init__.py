bcec88d827551e6232596abf048fdf919d420381
khaldi505 <habetlbrika69@gmail.com>
2021-08-27 01:23:47 +0100

    session auth

--- 0x07-Session_authentication/api/v1/views/__init__.py
+++ 0x07-Session_authentication/api/v1/views/__init__.py
@@ -4,7 +4,7 @@
 from flask import Blueprint
 from api.v1.views.index import *
 from api.v1.views.users import *
-from api.v1.auth.session_auth import *
+from api.v1.views.session_auth import *
 
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
