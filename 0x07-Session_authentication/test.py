ca7398b6f4b61f157ff6289da1e54f507698447d
khaldi505 <habetlbrika69@gmail.com>
2021-08-27 02:01:03 +0100

    updates

--- 0x07-Session_authentication/test.py
+++ 0x07-Session_authentication/test.py
@@ -14,10 +14,50 @@ from os import getenv
 from flask import jsonify, abort, request
 from api.v1.views import app_views
 
+
+class SessionAuth(Auth):
+    """
+    just inherits from auth
+    """
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+            session auth
+        """
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns a User ID based on a Session ID.
+        """
+        if session_id is None:
+            return None
+        if type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """
+        Returns a User instance based on a cookie value.
+        """
+        cookie_value = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(cookie_value)
+        user = User.get(user_id)
+        return user
+
+
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def auth_session() -> str:
     """
-        create an auth session
+        on the process of fugring out
     """
     email = request.form.get("email", None)
     password = request.form.get("password", None)
