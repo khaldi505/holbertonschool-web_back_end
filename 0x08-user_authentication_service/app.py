e64b48d10a355888d25de5d71fedccb2f60fcf30
khaldi505 <habetlbrika69@gmail.com>
2021-08-29 17:54:33 +0100

    flask create a user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -4,9 +4,12 @@ flask app
 """
 import flask
 from flask import Flask
+from flask.globals import request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
@@ -17,6 +20,26 @@ def index():
         only allow get request
     """
     return flask.jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """
+        an end point to register a user.
+
+    """
+    try:
+        user = AUTH.register_user(
+            request.form['email'],
+            request.form['password']
+                )
+    except ValueError:
+        return flask.jsonify({"message": "email already registered"}), 400
+    return flask.jsonify(
+        {
+            "email": "<registered email>",
+            "message": "user created"
+            })
 
 
 if __name__ == "__main__":
