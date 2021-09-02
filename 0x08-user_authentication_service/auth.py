2da1d9bd07d8d8fd87375d0a91397a1ec1a55809
khaldi505 <habetlbrika69@gmail.com>
2021-09-02 04:02:37 +0100

    task 18

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -124,3 +124,16 @@ class Auth:
             return user.reset_token
         except Exception as e:
             raise(ValueError)
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+            update password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            salt = bcrypt.gensalt()
+            hashed_pwd = bcrypt.hashpw(password, salt)
+            self._db.update_user(user.id, hashed_password=hashed_pwd)
+            self._db.update_user(user.id, reset_token=None)
+        except Exception as e:
+            raise(ValueError)
