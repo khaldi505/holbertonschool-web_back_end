f6d12fcedefe1b3f02f9c9fbd23add5d16aec0d1
khaldi505 <habetlbrika69@gmail.com>
2021-09-02 02:52:20 +0100

    task 15 profile endpoint

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -74,5 +74,17 @@ def logout():
     return redirect(url_for('index'))
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """
+    gets profile
+    """
+    user = AUTH.get_user_from_session_id(request.cookies.get('session_id'))
+    if user:
+        return jsonify({'email': '{}'.format(user.email)})
+    else:
+        flask.abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
