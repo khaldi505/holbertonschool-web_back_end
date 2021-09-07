a6b0c4d6f62396b131137a73696d97ab86d6f9fe
khaldi505 <habetlbrika69@gmail.com>
2021-09-07 14:45:08 +0100

    5-valid_email.sql

--- 0x0C-MySQL_Advanced/5-valid_email.sql
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER rest_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-IF STRCMP(NEW.email, OLD.email)=1
+IF NEW.email <> OLD.email
 THEN SET NEW.valid_email = 0;
 END IF;
 END $$
