f39e765b1f87fcaeb1adea0068f84865858034b9
khaldi505 <habetlbrika69@gmail.com>
2021-09-08 14:41:33 +0100

    0-app.py

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -1,8 +1,14 @@
+#!/usr/bin/env python3
+"""
+basic hello world example
+"""
 import flask
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET'])
 def hello_world():
+    """hello world"""
     return render_template('0-index.html')
