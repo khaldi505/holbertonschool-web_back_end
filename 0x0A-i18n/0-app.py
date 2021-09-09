0a3c4a40798939182e7144b8dd56b19b92087f43
khaldi505 <habetlbrika69@gmail.com>
2021-09-09 17:20:06 +0100

    paramtrizing template

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -4,9 +4,16 @@ basic hello world example
 """
 import flask
 from flask import Flask, render_template
-
+from flask_babel import Babel
 app = Flask(__name__)
+babel = Babel(app)
 
+
+class Config(object):
+    """
+    a configuration variable
+    """
+    LANGUAGES = ['en', 'fr']
 
 @app.route("/", methods=['GET'])
 def hello_world():
