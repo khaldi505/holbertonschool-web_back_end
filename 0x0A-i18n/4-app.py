173c3bfa5833dffa9bd06eea2a934919d05db8c5
khaldi505 <habetlbrika69@gmail.com>
2021-09-09 17:42:30 +0100

    force locale with url parameter, task 4

---
+++ 0x0A-i18n/4-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+ Parametrize templates
+"""
+import flask
+from flask import Flask, render_template, g, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    a configuration variable
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+@babel.localeselector
+def get_locale():
+    """ if a user is logged in, use the locale from the user settings
+    """
+    if request.args.get('locale'):
+        if request.args.get('locale') in Config.LANGUAGES:
+            return request.args.get('locale')
+    user = getattr(g, 'user', None)
+    if user is not None:
+        return user.locale
+    return request.accept_languages.best_match(['en', 'fr'])
+
+
+app.config.from_object(Config)
+
+
+@app.route("/", methods=['GET'])
+def hello_world():
+    """hello world"""
+    return render_template('3-index.html')
