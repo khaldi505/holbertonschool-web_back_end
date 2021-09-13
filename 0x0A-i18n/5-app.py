a34a03ab99e2002f026bd7f014ecac2f6159f465
khaldi505 <habetlbrika69@gmail.com>
2021-09-13 16:24:37 +0100

    nyes task 5

--- 0x0A-i18n/5-app.py
+++ 0x0A-i18n/5-app.py
@@ -35,6 +35,9 @@ def get_locale():
     if request.args.get('locale'):
         if request.args.get('locale') in Config.LANGUAGES:
             return request.args.get('locale')
+    user = getattr(g, 'user', None)
+    if user is not None:
+        return user.locale
     return request.accept_languages.best_match(['en', 'fr'])
 
 
