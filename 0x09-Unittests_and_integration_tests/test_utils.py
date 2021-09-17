58b7b0c707f494932d409d817d513daa37daacc3
khaldi505 <habetlbrika69@gmail.com>
2021-09-17 14:03:21 +0100

    test_utils task 1

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -22,6 +22,21 @@ class TestAccessNestedMap(unittest.TestCase):
                             ])
     def test_access_nested_map(self, nested_map, path, expected):
         """
-        test something
+        Assert the output is
+        equal to the expected result.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+       ({}, ('a', )),
+       ({'a': 1}, ('a', 'b'))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        test if the nested map function
+        raises the KeyError when an
+        argument, expected result is missing.
+        also makes sure that the exception message
+        is as expected
+        """
+        self.asserRaises(KeyError, access_nested_map, nested_map, path)
