3a4354726029dd1dcfaa3feaa2812d4441daece2
khaldi505 <habetlbrika69@gmail.com>
2021-09-19 10:59:46 +0100

    task doneé

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -4,8 +4,10 @@ unittest class to asure that
 Access nested map function
 works as expected
 """
-from utils import access_nested_map
+import utils
+from utils import access_nested_map, get_json
 import unittest
+from unittest.mock import patch
 from parameterized import parameterized, parameterized_class
 
 
@@ -40,3 +42,23 @@ class TestAccessNestedMap(unittest.TestCase):
         is as expected
         """
         self.assertRaises(KeyError,  access_nested_map, nested_map, path)
+
+    class TestGetJson(unittest.TestCase):
+        """
+        test get json
+        """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """
+            patch the requests.get
+            to return a fake payload
+            assert that we're getting
+            the expected result
+        """
+        with patch('utils.requests.get') as mc:
+            mc.return_value.json.return_value = test_payload
+            response = get_json(test_url)
+            self.assertEqual(response, test_payload)
