9b36cf81970cea38e40c5e56cdfc7400c4067dd5
khaldi505 <habetlbrika69@gmail.com>
2021-09-19 11:14:16 +0100

    task 2 press f for indet merci python

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -43,23 +43,23 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertRaises(KeyError,  access_nested_map, nested_map, path)
 
-    class TestGetJson(unittest.TestCase):
+class TestGetJson(unittest.TestCase):
+    """
+    test get json
+    """
+    @parameterized.expand([
+        ("http://example.com", {"test_payload": True}),
+        ("http://holberton.io", {"test_payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
         """
-        test get json
+            patch the requests.get
+            to return a fake payload
+            assert that we're getting
+            the expected result
         """
-        @parameterized.expand([
-            ("http://example.com", {"test_payload": True}),
-            ("http://holberton.io", {"test_payload": False})
-        ])
-        def test_get_json(self, test_url, test_payload):
-            """
-                patch the requests.get
-                to return a fake payload
-                assert that we're getting
-                the expected result
-            """
-            with patch('requests.get') as mc:
-                mc.return_value.json.return_value = test_payload
-                response = get_json(test_url)
-                self.assertEqual(response, test_payload)
-                mc.assert_called_once()
+        with patch('requests.get') as mc:
+            mc.return_value.json.return_value = test_payload
+            response = get_json(test_url)
+            self.assertEqual(response, test_payload)
+            mc.assert_called_once()
