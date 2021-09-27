3be2fa97b45684ff793bd3961faf75b61305b486
khaldi505 <habetlbrika69@gmail.com>
2021-09-27 10:02:17 +0100

    fixing value type to const

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   let counter = 0;
-  for (let value of array) {
+  for (const value of array) {
     array[counter] = appendString + value;
     counter += 1;
   }
