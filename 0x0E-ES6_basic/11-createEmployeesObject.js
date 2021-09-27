8dc29bf36b96361160b07d53d18c7b21837e117c
khaldi505 <habetlbrika69@gmail.com>
2021-09-27 11:32:11 +0100

    11-createEmployeesObject.js

--- 0x0E-ES6_basic/11-createEmployeesObject.js
+++ 0x0E-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
-    let result = {
-        [`${departmentName}`]: employees
-    }
-    return result
-   }
+  let result = {
+    [`${departmentName}`]: employees
+  }
+  return result
+}
