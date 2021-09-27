bab8314de7bd83ec172449f7aa9b839c095b47c2
khaldi505 <habetlbrika69@gmail.com>
2021-09-27 15:33:13 +0100

    task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+async_generator module
+"""
+
+
+import asyncio
+import random
+async def async_generator():
+    """
+    async_generator
+    """
+    for x in range(10):
+        await asyncio.sleep(1)
+        yield(random.uniform(0, 10))
