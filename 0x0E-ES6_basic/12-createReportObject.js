d695f3477a29f5d415d8399d8a0f5425386fdbd8
khaldi505 <habetlbrika69@gmail.com>
2021-09-27 14:14:40 +0100

    12-createReportObject.js

--- 0x0E-ES6_basic/12-createReportObject.js
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -1,6 +1,5 @@
 export default function createReportObject(employeesList) {
-  const getNumberOfDepartments;
-  getNumberOfDepartments = () => Object.keys(employeesList).length;
+  const getNumberOfDepartments = () => Object.keys(employeesList).length;
   const result = {
     allEmployees: employeesList,
     getNumberOfDepartments,
