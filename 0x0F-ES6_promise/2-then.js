191d4849d5c5c7f689ba13e7a243a2ee912b93e3
khaldi505 <habetlbrika69@gmail.com>
2021-10-02 11:58:04 -0700

    2-then.js

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,5 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   console.log('Got a response from the API');
-  return promise.then(() => ({ status: 200, body: 'Success',
-  })).catch(() => { new Error() });
+  return promise.then(() => ({
+    status: 200,
+    body: 'Success',
+  })).catch(() => new Error());
 }
