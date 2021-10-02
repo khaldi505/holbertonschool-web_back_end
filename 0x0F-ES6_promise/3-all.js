a8271b93c8b0aeb92013b0a1559eabdb7e69fab4
khaldi505 <habetlbrika69@gmail.com>
2021-10-02 13:40:36 -0700

    3-all.js

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -2,8 +2,8 @@ import {uploadPhoto, createUser} from './utils'
 
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
     console.log(values[0].body + " " + values[1].firstName + " " + values[1].lastName);
-  }).catch(Error('Signup system offline'));
+  }).catch(() => {console.log('Signup system offline')});
 }
