6fd67793433bfbfacb3971b3448f74db860cc3fd
khaldi505 <habetlbrika69@gmail.com>
2021-10-04 11:01:35 -0700

    3-currency.js

---
+++ 0x10-ES6_classes/3-currency.js
@@ -0,0 +1,27 @@
+export default class Currency {
+  constructor(code, name){
+    if (typeof(name) === 'string') { this._name = name }
+    if (typeof(code) === 'string') { this._code = code }
+  }
+
+  set name(name) {
+    if (typeof(name) === 'string') { this._name = name }
+  }
+
+  get name() {
+    return this._name
+  }
+
+  set code(code) {
+    if (typeof(code) === 'string') { this._code = code }
+  }
+
+  get code(){
+    return this.this._code
+  }
+
+  displayFullCurrency(){
+    return (`${this._name} (${this._code})`)
+  }
+
+}
