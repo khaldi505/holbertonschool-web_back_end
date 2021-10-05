2b49715e4ea8224a39e736da33d8de3cf7122d23
Iheb khaldi <ihebkhaldi505@gmail.com>
2021-10-05 14:25:22 -0700

    Create 7-airport.js

---
+++ 0x10-ES6_classes/7-airport.js
@@ -0,0 +1,9 @@
+export default class Airport {
+    constructor(name, code){
+        this._name = name
+        this._code = code
+    }
+    toString() {
+        return `[${typeof this} ${this._code}]`;
+      }
+}
