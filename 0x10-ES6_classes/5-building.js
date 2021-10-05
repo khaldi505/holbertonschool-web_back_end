087cd9d1331bce973a51b9d11ba9f6cfbc1c8bff
Iheb khaldi <ihebkhaldi505@gmail.com>
2021-10-05 11:45:28 -0700

    task 5 fixed constructor

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -1,14 +1,13 @@
 export default class Building {
 constructor(sqft){
+this._sqft = sqft
 if (this.constructor !== Building && !this.evacuationWarningMessage) throw Error('Class extending Building must override evacuationWarningMessage');
-if (typeof(sqft) !== "number") { this._sqft = sqft }
-
 }
 get sqft(){
 return this._sqft
 }
 set sqft(sqft){
-if (typeof(sqft) !== "number") { this._sqft = sqft }
+ this._sqft = sqft
 }
 
 }
