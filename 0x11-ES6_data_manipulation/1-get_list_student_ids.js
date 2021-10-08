06df9825d913332aa94958fb4de43bebf4e5df88
khaldi505 <habetlbrika69@gmail.com>
2021-10-08 14:13:39 +0100

    1-get_list_student_ids.js

---
+++ 0x11-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,6 @@
+export default function getListStudentIds(getListStudents){
+  if (Array.isArray(getListStudents)){
+    return getListStudents.map((element) => element.id)
+  }
+  return Array()
+}
