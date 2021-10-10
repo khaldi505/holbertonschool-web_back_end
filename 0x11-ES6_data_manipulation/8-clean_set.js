c1791d9810fba4e7d9ed1c81cb3c2fdadedd9bb5
khaldi505 <habetlbrika69@gmail.com>
2021-10-10 16:38:43 +0100

    8-clean_set.js

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,13 @@
 export default function cleanSet(set, startString){
-let counter = 0
+let counter = 2
 let result = "" 
 set.forEach(element => {
-    if (element.startsWith(startString) && startString !== "") {
-        if (counter === set.size - 1){
-            result += element.slice(startString.length, element.length)
-        } else { result += element.slice(startString.length, element.length) + "-" }
-        counter += 1;
-    }
+if (element && element.startsWith(startString) && startString !== "") {
+if (counter === set.size){
+result += element.slice(startString.length, element.length)
+} else { result += element.slice(startString.length, element.length) + "-" }
+counter += 1;
+}
 })
-    return result
+return result
 }
