85725d02df68bed872bc9336176b87b37cee1d8a
khaldi505 <habetlbrika69@gmail.com>
2021-10-11 16:22:47 +0100

    2-read_file.js

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -13,10 +13,8 @@ function countStudents(path) {
         result[1].push(resultElement[0]);
       } else if (resultElement[3] === 'SWE') { result[2].push(resultElement[0]); }
     });
-
-    console.log(result[0]);
-    console.log(`Number of students in CS: ${result[1].length}. List: ${result[1].toString().split(',').join(', ')}`);
-    console.log(`Number of students in SWE: ${result[2].length}. List: ${result[2].toString().split(',').join(', ')}`);
+    const finalResult = `${result[0]}\nNumber of students in CS: ${result[1].length}. List: ${result[1].toString().split(',').join(', ')}\nNumber of students in SWE: ${result[2].length}. List: ${result[2].toString().split(',').join(', ')}`;
+    console.log(finalResult);
   } catch (err) {
     if (err.code === 'ENOENT') {
       throw new Error('Cannot load the database');
