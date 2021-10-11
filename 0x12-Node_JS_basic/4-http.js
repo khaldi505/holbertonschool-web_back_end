49703a457dca39f764b8d9a1ab366b04a8a6a971
khaldi505 <habetlbrika69@gmail.com>
2021-10-11 22:51:48 +0100

    4-http.js

---
+++ 0x12-Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+module.exports = app.listen(port, hostname);
