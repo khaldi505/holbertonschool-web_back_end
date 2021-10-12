203defea5e555a4da88b73f975c875cbb4ea3af6
khaldi505 <habetlbrika69@gmail.com>
2021-10-12 21:27:07 +0100

    5-http.js

---
+++ 0x12-Node_JS_basic/5-http.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+const countStudents = require('./3-read_file_async');
+
+const filePath = process.argv[2];
+
+const promise = countStudents(filePath);
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/students') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    promise.then((data) => {
+      res.end(`This is the list of our students\n${data}`);
+    }).catch();
+  } else if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  }
+});
+
+module.exports = app.listen(port, hostname);
