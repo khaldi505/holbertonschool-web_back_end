fa1dbb0d2a35d6660baca5ed2c4646dc3957c284
khaldi505 <habetlbrika69@gmail.com>
2021-10-12 22:36:53 +0100

    fixing eslint coding style"

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,14 +1,14 @@
-import signUpUser from './4-user-promise'
-import uploadPhoto from './5-photo-reject'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup (firstName, lastName, fileName) {
-  const result = []
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const result = [];
   await signUpUser(firstName, lastName)
-    .then((value) => { result.push({ status: 'fulfilled', value }) })
-    .catch(() => result.push({ status: 'rejected' }))
+    .then((value) => { result.push({ status: 'fulfilled', value }); })
+    .catch(() => result.push({ status: 'rejected' }));
   await uploadPhoto(fileName)
-    .then((value) => { result.push({ status: 'fulfilled', value }) })
-    .catch((error) => result.push({ status: 'rejected', value: error.toString() }))
+    .then((value) => { result.push({ status: 'fulfilled', value }); })
+    .catch((error) => result.push({ status: 'rejected', value: error.toString() }));
   // const result = [signUpUser(firstName, lastName), uploadPhoto(fileName)]
-  return result
+  return result;
 }
