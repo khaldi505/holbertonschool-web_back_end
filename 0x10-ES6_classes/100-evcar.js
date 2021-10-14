21a5357b51066383c646ddecec544205eab54c42
khaldi505 <habetlbrika69@gmail.com>
2021-10-14 16:46:55 +0100

    100-evcar.js

--- 0x10-ES6_classes/100-evcar.js
+++ 0x10-ES6_classes/100-evcar.js
@@ -3,8 +3,13 @@ import Car from './10-car';
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
     this._range = range;
   }
 
-  static get [Symbol.species]() { return Car; }
+  cloneCar() {
+    return Object.assign(new Car(), this.constructor);
+  }
 }
