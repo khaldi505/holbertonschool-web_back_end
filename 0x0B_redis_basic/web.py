b68ec5a4fec40ec81ba123bd7f1e63b8d5c8315a
khaldi505 <habetlbrika69@gmail.com>
2021-10-16 22:33:49 +0100

    web.py

---
+++ 0x0B_redis_basic/web.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+    Cache class that stores instance of the redis
+    client as a private variable named
+    _redis using redis.Redist()
+    and flush the instance using flushdb.
+"""
+
+import redis
+import requests
+from functools import wraps
+from typing import Callable
+
+_redis = redis.Redis()
+
+
+def counter(method: Callable) -> Callable:
+    """
+    """
+    @wraps(method)
+    def wrapper(url):
+        _redis.incr("count:{}".format(url))
+        result = method(url)
+        redis.set("{}".format(result), time_to_expire_s=10)
+        return result
+    return(wrapper)
+
+
+@counter
+def get_page(url: str) -> str:
+    """
+    """
+    return requests.get(url).text
