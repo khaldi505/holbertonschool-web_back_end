72200901806f857d31ee0cd8ca5cabf99ab5ddd2
khaldi505 <habetlbrika69@gmail.com>
2021-10-16 15:29:50 +0100

    task 3

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -29,7 +29,7 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
-def call_history(method: Callable) -> callable:
+def call_history(method: Callable) -> Callable:
     """
     store the history of inputs and
     outputs for a particular function.
