abe9510eb5f331cef400e3682cfa52c52d650931
khaldi505 <habetlbrika69@gmail.com>
2021-10-18 10:15:22 +0100

    7-average_score.sql

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -1,8 +1,6 @@
 -- creates a stored procedure ComputeOverallScoreForUser
 DELIMITER $$
-CREATE PROCEDURE ComputeOverallScoreForUser (
-    IN user_id INT
-)
+CREATE PROCEDURE ComputeOverallScoreForUser (IN user_id INT)
 BEGIN
     UPDATE users
     SET average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
