a600ba0f75c50fd30833a9002b2227ae32420b2a
khaldi505 <habetlbrika69@gmail.com>
2021-10-24 16:14:15 +0100

    fixing coding style

--- 0x14-queuing_system_in_js/1-redis_op.js
+++ 0x14-queuing_system_in_js/1-redis_op.js
@@ -1,24 +1,23 @@
-import redis from "redis";
+import redis from 'redis';
+
 const client = redis.createClient();
 
-client.on("error", function(err) {
-  console.log("Redis client not connected to the server: " + err.message);
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
 });
-client.on("connect", function() {
-  console.log("Redis client connected to the server");
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
 });
 
-
-function setNewSchool(schoolName, value){
+function setNewSchool(schoolName, value) {
   client.set(schoolName, value, redis.print);
 }
 
-function displaySchoolValue(schoolName){
-  client.get(schoolName, function(err, rep){
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, rep) => {
     console.log(rep);
   });
 }
-
 
 displaySchoolValue('Holberton');
 setNewSchool('HolbertonSanFrancisco', '100');
