ea86066dbddd7e52d2892af1f2ff774a477504f2
khaldi505 <habetlbrika69@gmail.com>
2021-10-24 17:28:38 +0100

    task 8

--- 0x0D-NoSQL/8-all.py
+++ 0x0D-NoSQL/8-all.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+"""
+
+
+def list_all(mongo_collection):
+    """
+    """
+    result = mongo_collection.school.find()
+    if result:
+        return result
+    return []
