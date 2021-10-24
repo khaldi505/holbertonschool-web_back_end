3b03b025a0049327fe311efccdbe06e3450a7de1
khaldi505 <habetlbrika69@gmail.com>
2021-10-24 15:59:51 +0100

    0-redis_client.js

--- 0x14-queuing_system_in_js/0-redis_client.js
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -1,11 +1,9 @@
-import redis from 'redis';
-
+import redis from "redis";
 const client = redis.createClient();
 
-client.on('error', (err) => {
-  console.log('Redis client not connected to the server: ', err.message);
+client.on("error", function(err) {
+  console.log("Redis client not connected to the server: " + err.message);
 });
-
-client.on('connect', () => {
-  console.log('Redis client connected to the server');
+client.on("connect", function() {
+  console.log("Redis client connected to the server");
 });
