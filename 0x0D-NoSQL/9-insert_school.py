5663471b0c21ebeea1c535fd6ddda54c92cb4565
khaldi505 <habetlbrika69@gmail.com>
2021-10-24 17:32:24 +0100

    9-insert_school.py

--- 0x0D-NoSQL/9-insert_school.py
+++ 0x0D-NoSQL/9-insert_school.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    """
+    collection = mongo_collection
+    inserted = collection.insert_one(kwargs).inserted_id
+    return inserted
