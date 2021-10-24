4b5ece3a1c5b762d3cc21b6f31be7e366c44d049
khaldi505 <habetlbrika69@gmail.com>
2021-10-24 17:42:18 +0100

    10-update_topics.py

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    """
+    result = mongo_collection.update_many(
+        {"name": name}, {'$set': {'topics': topics}})
+    return result
